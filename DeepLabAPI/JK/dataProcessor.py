80def3dc493bdf462e668356c10f1e7b149846b4
Jangyoon Kim <jangyoon107@kaist.ac.kr>
2019-11-21 12:35:54 +0900

    Converting to grayscale

--- DeepLabAPI/JK/dataProcessor.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import glob
-from PIL import Image
-
-def dataProcessor(filePath, outputPath):
-    origImages = glob.glob(filePath + '/*.jpg')
-    segImages = glob.glob(filePath + '/*_seg.png')
-    
-    
